7d687cf8f6c6709e2946ecfa1247dbeff1c6f9dd
newtonneto <newton.mgs@gmail.com>
2019-05-18 15:44:29 -0300

    L22E04

---
+++ Lista 22/L22E04/L22E04/PontuacaoInferiorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L21E04
+{
+    class PontuacaoInferiorException : Exception
+    {
+        public PontuacaoInferiorException()
+        {
+
+        }
+        public PontuacaoInferiorException(string message) : base(message)
+        {
+
+        }
+        PontuacaoInferiorException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
